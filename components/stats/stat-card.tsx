29d08023f1fb6afbf6676264ec338f985158741c
Florian Heysen <heysen.florian@gmail.com>
2024-09-08 15:05:26 +0200

    fix apos

--- components/stats/stat-card.tsx
+++ components/stats/stat-card.tsx
@@ -17,7 +17,7 @@ export default function StatCard({ title, value, percentageChange, icon }: StatC
             </CardHeader>
             <CardContent>
                 <div className="text-2xl font-bold">{value}</div>
-                <p className="text-xs text-muted-foreground">{percentageChange} de l'année dernière</p>
+                <p className="text-xs text-muted-foreground">{percentageChange} de l&apos;année dernière</p>
             </CardContent>
         </Card>
     );
